fa2c411e5c3e9633932669af255af0bb990a5a68
David Whittington <djwhitt@gmail.com>
2021-08-06 14:09:59 -0500

    Cleanup Arweave argument string generation

--- src/net/strongpool/node/tasks.clj
+++ src/net/strongpool/node/tasks.clj
@@ -1,28 +1,29 @@
 (ns net.strongpool.node.tasks
-  (:require
-   [babashka.process :refer [check process]]
-   [clojure.java.io :as io]
-   [clojure.pprint :refer [pprint]]
-   [clojure.string :as str]
-   [net.strongpool.node.config :as config]
-   [net.strongpool.node.rcf-shim :refer [tests]]))
+  (:require [babashka.process :refer [check process]]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as str]
+            [net.strongpool.node.config :as config]
+            [net.strongpool.node.rcf-shim :refer [tests]]))
 
-;; TODO generate vector rather than string of args
 (defn arweave-args [config]
-  (cond-> ""
-    (:mine? config)
-    (str " mine")
+  (let [{:keys [mine? miner-address arweave]} config
+        {:keys [extra-args peers]} arweave]
+    (cond-> []
+      mine?
+      (conj "mine")
 
-    (and (:mine? config) (:miner-address config))
-    (str " mining_addr " (:miner-address config) " ")
+      miner-address
+      (into ["mining_addr" miner-address])
 
-    (get-in config [:arweave :extra-args])
-    (str (str/join " " (get-in config [:arweave :extra-args])) " ")
+      extra-args
+      (into extra-args)
 
-    (get-in config [:arweave :peers])
-    (str (->> (get-in config [:arweave :peers])
-              (str/join " peer ")
-              (str "peer ")))))
+      peers
+      (into (interleave (repeat "peer") peers)))))
+
+(defn arweave-args-str [config]
+  (str/join " " (arweave-args config)))
 
 (defn arweave-env-vars [config]
   (let [{:keys [debug?]} config
@@ -38,7 +39,7 @@
 
 (defn start []
   (when-let [config (config/validated-load)]
-    (let [args (arweave-args config)]
+    (let [args (arweave-args-str config)]
       (println "Starting Strongpool node... ")
       ;; TODO stream output
       (-> (process ["docker-compose" "up" "-d"]
@@ -68,6 +69,7 @@
     ;; TODO wait for arweave service to actually stop
     (println "Strongpool node stopped.")))
 
+;; TODO why does this end up clearing the screen part way through
 (defn logs []
   (let [p (process ["docker-compose" "logs"]) ]
     (with-open [rdr (io/reader (:out p))]
@@ -86,7 +88,31 @@
 
 (tests
 
-  "generate minimal arweave args"
-  (arweave-args {:mine? true}) := " mine"
+ ;; Arweave args tests
 
-  )
+ (def empty-config {})
+
+ (arweave-args empty-config) := []
+
+ (def miner-config {:mine? true
+                    :miner-address "Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c"})
+
+ (arweave-args miner-config) :=
+ ["mine" "mining_addr" "Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c"]
+
+ (def config-with-peers (-> miner-config
+                            (assoc-in [:arweave :peers] #{"188.166.200.45"
+                                                          "188.166.192.169"})))
+
+ (arweave-args config-with-peers) :=
+ ["mine" "mining_addr" "Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c"
+  "peer" "188.166.192.169" "peer" "188.166.200.45"]
+
+ (def config-with-extra-args (-> miner-config
+                                 (assoc-in [:arweave :extra-args] ["sync_jobs" "10"])))
+
+ (arweave-args config-with-extra-args) :=
+ ["mine" "mining_addr" "Tk1NuG7Jxr9Ecgva5tWOJya2QGDOoS6hMZP0paB129c"
+  "sync_jobs" "10"]
+
+ )
